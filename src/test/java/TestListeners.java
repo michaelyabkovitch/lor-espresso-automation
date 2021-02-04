afe71b8d27f0f174d046ffd7378c2ca41482f260
michal <michael.yav@gmail.com>
2021-02-04 13:16:25 +0200

    1 st commit

---
+++ src/test/java/TestListeners.java
@@ -0,0 +1,52 @@
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.service.ExtentService;
+import com.aventstack.extentreports.service.ExtentTestManager;
+import net.bsmch.Screenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+
+public class TestListeners implements ITestListener {
+    @Override
+    public void onTestStart(ITestResult iTestResult) {
+        ExtentTestManager.createMethod(iTestResult);
+
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult iTestResult) {
+        ExtentTestManager.log(iTestResult);
+
+    }
+
+    @Override
+    public void onTestFailure(ITestResult iTestResult) {
+        WebDriver driver = (WebDriver) iTestResult.getTestContext().getAttribute("driver");
+        ExtentTestManager.log(iTestResult);
+        ExtentTestManager.getTest().log(Status.FAIL,
+                iTestResult.getName(),
+                Screenshot.captureScreen(iTestResult.getName(), driver));
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onStart(ITestContext iTestContext) {
+
+    }
+
+    @Override
+    public void onFinish(ITestContext iTestContext) {
+        ExtentService.getInstance().flush();
+    }
+}
