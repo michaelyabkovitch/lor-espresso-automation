3269ee5f58edd68e3e2746e73f3e2d1d6ca58cdc
Your Name <you@example.com>
2021-02-10 21:39:33 +0200

    first add

--- src/test/java/Pages/BasePage.java
+++ src/test/java/Pages/BasePage.java
@@ -1,4 +1,20 @@
 package Pages;
 
-public class BasePage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage extends PageObject{
+    // Constructor
+    public BasePage(WebDriver driver) {
+        super(driver);
+    }
+
+    public BasePage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+
+    // WebElements definition
+
 }
--- src/test/java/Pages/PageObject.java
+++ src/test/java/Pages/PageObject.java
@@ -1,4 +1,22 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class PageObject {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    // Constructors
+    public PageObject(WebDriver driver) {
+        this(driver, new WebDriverWait(driver, 10));
+    }
+
+    public PageObject(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+
+        PageFactory.initElements(driver, this);
+    }
 }
