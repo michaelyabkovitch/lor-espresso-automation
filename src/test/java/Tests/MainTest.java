7aca259f6691089695fa91db39b726490f6c883e
michal <michael.yav@gmail.com>
2021-02-18 08:30:05 +0200

    1)add file creatore. 2)change to main test

--- src/test/java/Tests/MainTest.java
+++ src/test/java/Tests/MainTest.java
@@ -1,5 +1,6 @@
 package Tests;
 
+import Pages.PageObject;
 import atu.testrecorder.ATUTestRecorder;
 import atu.testrecorder.exceptions.ATUTestRecorderException;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -10,6 +11,8 @@ import org.testng.ITestContext;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
+
+import java.io.File;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -21,9 +24,14 @@ public class MainTest {
     public WebDriver driver;
     public WebDriverWait wait;
     final String RECORD_PATH = "./records";
+    //
+    File theDir = new File("./records");
+    FillCreator FillCreator = new FillCreator();
+
 
     @BeforeTest
     public void initialize(ITestContext iTestContext) throws ATUTestRecorderException {
+        FillCreator.Creator(theDir);
         recorder = new ATUTestRecorder(RECORD_PATH,
                 "new record - " + dateFormat.format(date), false);
         recorder.start();
@@ -41,4 +49,6 @@ public class MainTest {
             recorder.stop();
         }
     }
+
+
 }
