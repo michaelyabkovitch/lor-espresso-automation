b4ced36ab965cb918280c2c09dab0a6fa57a1e91
michal <michael.yav@gmail.com>
2021-03-15 22:32:35 +0200

    update to footer page and test update to base page

--- src/test/java/Pages/FooterPage.java
+++ src/test/java/Pages/FooterPage.java
@@ -1,14 +1,101 @@
 package Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
 
-public class FooterPage extends BasePage {
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class FooterPage  extends BasePage {
     public FooterPage(WebDriver driver) {
+
         super(driver);
     }
 
     public FooterPage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
     }
+
+
+    // WebElements definition
+    @FindBy (name = "contactFirstName" )
+    WebElement contactFirstName;
+    @FindBy (name = "contactLastName" )
+    WebElement contactLastName;
+    @FindBy (name = "contactEmail" )
+    WebElement contactEmail;
+    @FindBy (name = "contactComment")
+    WebElement contactComment;
+    @FindBy (className = "list-item")
+    List<WebElement> listIteml ;
+    @FindBy (id  = "contact-topic")
+    WebElement contactTopicDropDownMenu;
+
+    //
+    @FindBy(className = "faq-title")
+    WebElement title;
+
+
+    // Variable definition
+    SoftAssert softAssert = new SoftAssert();
+
+
+    private  String returnWebElement(String value) throws InterruptedException {
+        js.executeScript("window.scrollBy(0,5000)");
+        String element = null;
+        for (int i = 0; i < listIteml.size(); i++) {
+            if (listIteml.get(i).getText().equalsIgnoreCase(value)) {
+                element = listIteml.get(i).getText();
+                System.out.println(listIteml.get(i).getText());
+                break;
+            }
+        }
+        return element;
+    }
+    public   void getLinkValue( String value) throws InterruptedException {
+        waitAndClick(driver.findElement(By.partialLinkText(returnWebElement(value))));
+
+    }
+
+
+    public void ContactUsFormFill () throws InterruptedException {
+        getLinkValue("צרו קשר");
+        FooterPage.sendKeys(contactFirstName,"Test");
+        FooterPage.sendKeys(contactLastName, "Test");
+        FooterPage.sendKeys(contactEmail, "Test@gmail.com");
+        Select mySelection = new Select(contactTopicDropDownMenu);
+        mySelection.selectByValue("ביטול הזמנה");
+        FooterPage.sendKeys(contactComment,"Test Test Test");
+
+
+
+    }
+
+
+    public void footerLinkTest () throws InterruptedException {
+        getLinkValue("המחירים באתר אינם כוללים דמי משלוח");
+        Assert.assertEquals(getTitle(),"המחירים באתר אינם כוללים דמי משלוח");
+        Assert.assertEquals(getTitle(),"המחירים באתר אינם כוללים דמי משלוח");
+
+    }
+
+    public String getTitle (){
+        return title.getText();
+
+
+
+
+    }
+
+
+
+
+
 }
--- src/test/java/Tests/FooterTest.java
+++ src/test/java/Tests/FooterTest.java
@@ -1,5 +1,48 @@
 package Tests;
 
+import Pages.FooterPage;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+
+
 public class FooterTest extends MainTest {
-    
+    FooterPage FooterPage;
+
+    @BeforeClass
+    public void before() {
+         FooterPage = new FooterPage(driver);
+    }
+
+
+    //    @Test
+//    public void ContactUsFormFill () throws InterruptedException {
+//        FooterPage.ContactUsFormFill();
+//    }
+//
+//    @Test
+//    public void footerLinkTest()throws InterruptedException {
+//        FooterPage.footerLinkTest();
+//
+//    }
+    @Test(dataProvider = "test-data")
+    public void blabls(String expactedValue) throws InterruptedException {
+
+        FooterPage.getLinkValue(expactedValue);
+        String actuallVale = FooterPage.getTitle();
+
+        Assert.assertEquals(actuallVale, expactedValue);
+
+    }
+
+    @DataProvider(name = "test-data")
+    public Object[][] dataProvFunc() {
+        return new Object[][]{
+                {"המחירים באתר אינם כוללים דמי משלוח"}, {"שירות המשלוחים"}
+        };
+    }
+
+
 }
